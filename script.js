30eb381bfa43ae2fd46c89c9f79efe1b14cadb11
NightmareShadow4Exploit <157217769+NightmareShadow4Exploit@users.noreply.github.com>
2024-09-20 21:47:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 const repoApiUrl = 'https://api.github.com/repos/NightmareShadow4Exploit/web/contents/';
 
-// Fetch files for each section
 const sections = [
-    { folder: 'Attendance', listId: 'attendanceFileList' },
-    { folder: 'MinutesOfMeeting', listId: 'minutesFileList' },
-    { folder: 'CheckList', listId: 'checklistFileList' },
-    { folder: 'PlanningAchievement', listId: 'planningFileList' },
-    { folder: 'Reports', listId: 'reportsFileList' },
-    { folder: 'Books', listId: 'booksFileList' },
-    { folder: 'Articles', listId: 'articlesFileList' },
-    { folder: 'SelfAssessment', listId: 'selfAssessmentFileList' },
+    { folder: 'attendance', listId: 'attendanceFileList' },
+    { folder: 'minutes-of-meeting', listId: 'minutesFileList' },
+    { folder: 'checklist', listId: 'checklistFileList' },
+    { folder: 'planning-achievement', listId: 'planningFileList' },
+    { folder: 'reports', listId: 'reportsFileList' },
+    { folder: 'books', listId: 'booksFileList' },
+    { folder: 'articles', listId: 'articlesFileList' },
+    { folder: 'self-assessment', listId: 'selfAssessmentFileList' },
 ];
 
-// Fetch files for each section
 sections.forEach(section => fetchFiles(section.folder, section.listId));
 
-// Fetch files from GitHub
 function fetchFiles(folder, listId) {
     fetch(`${repoApiUrl}${folder}`)
         .then(response => {
@@ -29,8 +26,6 @@ function fetchFiles(folder, listId) {
             data.forEach(file => {
                 if (file.type === 'file' && (file.name.endsWith('.pdf') || file.name.endsWith('.xlsx'))) {
                     const listItem = document.createElement('li');
-
-                    // Create the file link
                     const fileLink = document.createElement('a');
                     fileLink.href = file.download_url;
                     fileLink.textContent = file.name;
